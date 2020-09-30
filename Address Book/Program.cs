4891646fb459c9f91833ddbb66c8d0504d6f8157
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-01 00:09:47 +0530

    [pranav] Add. Add people to address book

--- Address Book/Program.cs
+++ Address Book/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Address_Book
 {
@@ -6,7 +7,36 @@ namespace Address_Book
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Solution!");
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            person1.setFirstName("A");
+            person1.setLastName("Z");
+            person1.setAddress("xxx");
+            person1.setCity("C1");
+            person1.setState("S1");
+            person1.setContactNo(123);
+            person1.setZip(1);
+            person1.setEmailId("a.z@gmail.com");
+
+            person2.setFirstName("B");
+            person2.setLastName("Y");
+            person2.setAddress("xxx");
+            person2.setCity("C2");
+            person2.setState("S2");
+            person2.setContactNo(567);
+            person2.setZip(2);
+            person2.setEmailId("b.y@gmail.com");
+
+            List<Person> people = new List<Person>();
+
+            people.Add(person1);
+            people.Add(person2);
+
+            foreach (var i in people)
+            {
+                Console.WriteLine(i.getLastName());
+            }
         }
     }
 }
