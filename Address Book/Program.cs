81e839d67d343d38280ea2f2a790169abce77b10
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-01 08:38:28 +0530

    [pranav] Add. Created Address Book

--- Address Book/Program.cs
+++ Address Book/Program.cs
@@ -7,60 +7,60 @@ namespace Address_Book
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person();
-            Person person2 = new Person();
+            //Person person1 = new Person();
+            //Person person2 = new Person();
 
-            person1.setFirstName("A");
-            person1.setLastName("Z");
-            person1.setAddress("xxx");
-            person1.setCity("C1");
-            person1.setState("S1");
-            person1.setContactNo(123);
-            person1.setZip(1);
-            person1.setEmailId("a.z@gmail.com");
-
-            person2.setFirstName("B");
-            person2.setLastName("Y");
-            person2.setAddress("xxx");
-            person2.setCity("C2");
-            person2.setState("S2");
-            person2.setContactNo(567);
-            person2.setZip(2);
-            person2.setEmailId("b.y@gmail.com");
+            //person1.setFirstName("A");
+            //person1.setLastName("Z");
+            //person1.setAddress("xxx");
+            //person1.setCity("C1");
+            //person1.setState("S1");
+            //person1.setContactNo(123);
+            //person1.setZip(1);
+            //person1.setEmailId("a.z@gmail.com");
+            
+            //person2.setFirstName("B");
+            //person2.setLastName("Y");
+            //person2.setAddress("xxx");
+            //person2.setCity("C2");
+            //person2.setState("S2");
+            //person2.setContactNo(567);
+            //person2.setZip(2);
+            //person2.setEmailId("b.y@gmail.com");
 
             List<Person> people = new List<Person>();
 
-            people.Add(person1);
-            people.Add(person2);
+            //people.Add(person1);
+            //people.Add(person2);
 
-            foreach (var i in people)
-            {
-                Console.WriteLine(i.getFirstName());
-                Console.WriteLine(i.getLastName());
-                Console.WriteLine(i.getCity());
-                Console.WriteLine(i.getState());
-                Console.WriteLine(i.getContactNo());
-                Console.Write("\n");
-            }
+            //foreach (var i in people)
+            //{
+                //Console.WriteLine(i.getFirstName());
+                //Console.WriteLine(i.getLastName());
+                //Console.WriteLine(i.getCity());
+                //Console.WriteLine(i.getState());
+                //Console.WriteLine(i.getContactNo());
+                //Console.Write("\n");
+            //}
 
-            Console.WriteLine("Details after editting details: ");
+            //Console.WriteLine("Details after editting details: ");
 
-            foreach (var i in people)
-            {
-                if(i.getFirstName().Equals("A"))
-                {
-                    people.Remove(i);
-                }
-            }
+            //foreach (var i in people)
+            //{
+                //if (i.getFirstName().Equals("A"))
+                //{
+                    //people.Remove(i);
+                //}
+            //}
 
-                foreach (var i in people)
-            {
-                Console.WriteLine(i.getFirstName());
-                Console.WriteLine(i.getLastName());
-                Console.WriteLine(i.getCity());
-                Console.WriteLine(i.getState());
-                Console.WriteLine(i.getContactNo());
-                Console.Write("\n");
+            //foreach (var i in people)
+            //{
+                //Console.WriteLine(i.getFirstName());
+                //Console.WriteLine(i.getLastName());
+                //Console.WriteLine(i.getCity());
+                //Console.WriteLine(i.getState());
+                //Console.WriteLine(i.getContactNo());
+                //Console.Write("\n");
             }
         }
     }
